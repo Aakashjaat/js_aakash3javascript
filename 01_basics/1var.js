0af91353d724e8c49ec7c6b8a23200501c5dacee
infinity047 <166687114+infinity047@users.noreply.github.com>
2024-06-25 09:46:47 +0000

    data types

---
+++ 01_basics/1var.js
@@ -0,0 +1,17 @@
+const accountId = 144552
+let accountEmail = "aakash@google.com"
+var accountPassword = "Jaipur"
+ 
+// accountID = 2
+
+console.log(accountId);
+
+accountEmail = "abc@.com"
+accountPassword = "2212"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+
+*/
---
+++ 01_basics/2datatypes.js
@@ -0,0 +1,23 @@
+let name = "aakash"
+let age = 18
+let isloggedIn= false
+let state;
+
+
+/*
+num =>2 to power 53
+bigilit
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+
+object
+
+*/
+console.log(typeof age);       // num
+console.log(typeof null);      //object
+console.log(typeof undefined);   //undefined
+
+
