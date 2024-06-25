55b70cefd60f8ba07667ac328fc9f4a6c4bc5f56
infinity047 <166687114+infinity047@users.noreply.github.com>
2024-06-25 10:16:31 +0000

    conversion

--- 01_basics/3conversionoperations.js
+++ 01_basics/3conversionoperations.js
@@ -8,3 +8,11 @@ let isLoggedIn = "1"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn );
 
+//******************** OPERATIONS *********************//
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
