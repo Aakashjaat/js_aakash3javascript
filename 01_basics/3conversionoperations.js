35977b42b7302c63b7eb94f8a575c53999cee7fc
infinity047 <166687114+infinity047@users.noreply.github.com>
2024-06-25 10:00:09 +0000

    conversion

---
+++ 01_basics/3conversionoperations.js
@@ -0,0 +1,10 @@
+//let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let isLoggedIn = "1"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn );
+
